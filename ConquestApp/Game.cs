55431276d0acee0589f279ae8b197d8064f1a27d
booch94 <booch1994@icloud.com>
2020-04-11 17:21:17 +0300

    add base entity class

---
+++ ConquestApp/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConquestLib.Entities;
+
+namespace ConquestApp
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Entity pesho = new Entity(100);
+            Console.WriteLine(pesho.Health);
+        }
+    }
+}
---
+++ ConquestLib/Entities/Entity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConquestLib.Entities
+{
+    public class Entity
+    {
+        private int health;
+        private bool alive;
+
+
+        public int Health
+        {
+            get
+            {
+                return health;
+            }
+            set
+            {
+                health += value;
+                if (health <= 0)
+                {
+                    alive = false;
+                    Console.WriteLine("You are dead");
+                }
+            }
+
+        }
+
+
+        public Entity(int initialHealth)
+        {
+            health = initialHealth;
+            alive = true;
+        }
+    }
+}
