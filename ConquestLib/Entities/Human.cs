62a6677ea9b38daac66c0cf00150c15b68113d18
booch94 <booch1994@icloud.com>
2020-04-12 18:24:55 +0300

    add target methods to human class

--- ConquestLib/Entities/Human.cs
+++ ConquestLib/Entities/Human.cs
@@ -9,6 +9,7 @@ namespace ConquestLib.Entities
         private string firstName;
         private string lastName;
         private int stamina;
+        private Entity target;
 
         public string FirstName 
         {
@@ -48,6 +49,18 @@ namespace ConquestLib.Entities
                 stamina = value;
             }
         }
+        public Entity Target
+        {
+            get
+            {
+                return target;
+            }
+            set
+            {
+                target = value;
+
+            }
+        }
 
         public Human(int initialHealth, string firstName, string lastName, int initialStamina) 
             : base (initialHealth)
@@ -56,6 +69,17 @@ namespace ConquestLib.Entities
             this.FirstName = firstName;
             this.LastName = lastName;
             this.Stamina = initialStamina;
+            this.Target = null;
+        }
+
+        public void SetTarget(Entity target)
+        {
+            this.Target = target;
+        }
+
+        public void ClearTarget()
+        {
+            this.Target = null;
         }
     }
 }
